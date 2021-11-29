2731ee2d108d6d5ac6b8add0eef75604567eefe4
C1FR1 <c1fr1.stkcps@gmail.com>
2021-11-29 00:11:09 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ABOUT
+
+This repository is used as a testing ground for [Enignets](https://github.com/c1fr1/Enignets). It also serves as a set
+of examples of how to use Enignets.
---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+/*
+ Should be one of the following depending on your operating system or the platform you are targeting
+
+ x64 Windows - "windows"
+ x86 Windows - "windows-x86"
+ arm64 Windows - "windows-arm64"
+
+ x64 Linux - "linux"
+ x64 Linux - "linux-arm64"
+ x64 Linux - "linux-arm32"
+ */
+val osString = "windows"
+val lwjglNatives = "natives-${osString}"
+
+//this can be any valid tag on this repo, or a short commit id. More info at https://jitpack.io/.
+val enignetsVersion = "v1.0.1a_1"
+
+val lwjglVersion = "3.2.3"
+val jomlVersion = "1.10.1"
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+group = "c1fr1"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.joml", "joml", jomlVersion)
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+
+    implementation("com.github.c1fr1:Enignets:${enignetsVersion}")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("AnimatedCollisionTestMainKt")
+}
